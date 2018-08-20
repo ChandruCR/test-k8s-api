ce4b2e77e58b4c51cb1c78ec584c4afb6c37754e
Chandru <chandrucrm@peopleintouch.nl>
2018-08-20 14:54:18 +0200

    added script

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,3 +1,4 @@
+console.log("Running app.js")
 const express = require('express');
 const request = require('request');
 const app = express();
@@ -7,8 +8,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const signUpService = "http://signup.hermes:8080"
 
+app.route('/')
+    .get(function (req, res) {
+        console.log(req)
+        var result = {
+            "test": "Hello World"
+        }
+        res.json(result)
+    })
 app.route('/signup')
     .post(function (req, res) {
+        console.log("request")
+        console.log(req);
         request.post(signUpService ).on('response', function(res1) {
             res1.on('data', function(data1) {
                 res.json(data1)
@@ -16,4 +27,5 @@ app.route('/signup')
         })
     })
 
+console.log("Listening on 3000")
 app.listen(3000);
