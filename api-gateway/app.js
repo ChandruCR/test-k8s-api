da159e3a9b7c7b4f1206ee371994ced2800497ed
Chandru <chandrucrm@peopleintouch.nl>
2018-08-21 14:15:16 +0200

    updated yaml

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     next()
 })
 
-app.post('/signup', (req, res, next) => {
+app.post('/add', (req, res, next) => {
     console.log("request")
     console.log(req);
     userServiceProxy(req, res, next)
